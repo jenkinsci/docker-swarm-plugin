c26c14c60f141fcb43c9eff80e6a693f4f1720d1
Surya Gaddipati <surya.gaddipati@gmail.com>
2016-04-22 10:30:44 -0500

    Add lock pid

--- src/main/golang/docker-cache-driver/main.go
+++ src/main/golang/docker-cache-driver/main.go
@@ -2,14 +2,39 @@ package main
 
 import (
 	"github.com/docker/go-plugins-helpers/volume"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"syscall"
 )
 
 func main() {
-	driver := newCacheDriverDriver()
+	WithLock("/var/run/cache-driver.pid", func() {
+		driver := newCacheDriverDriver()
 
-	handler := volume.NewHandler(driver)
-	err := handler.ServeUnix("root", driver.name)
+		handler := volume.NewHandler(driver)
+		err := handler.ServeUnix("root", driver.name)
+		if err != nil {
+			os.Exit(1)
+		}
+	})
+}
+func WithLock(pidFileName string, f func()) {
+	pidFile, err := os.Create(pidFileName)
 	if err != nil {
-		panic(err.Error())
+		os.Exit(1)
 	}
+	defer pidFile.Close()
+	err = ioutil.WriteFile(pidFileName, []byte(strconv.Itoa(os.Getpid())), 0755)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	err = syscall.Flock(int(pidFile.Fd()), syscall.LOCK_EX)
+	if err != nil {
+		os.Exit(1)
+	}
+	defer syscall.Flock(int(pidFile.Fd()), syscall.LOCK_UN)
+	f()
+
 }
