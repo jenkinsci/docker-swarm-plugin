6e0037ecda509b14ff201add4bef6f08fa2afc78
Surya Gaddipati <surya.gaddipati@gmail.com>
2016-07-28 18:51:00 -0500

    cleanup stage name

--- src/main/golang/docker-cache-driver/build_cache.go
+++ src/main/golang/docker-cache-driver/build_cache.go
@@ -44,26 +44,26 @@ func (buildCache *buildCache) destroy(driver cacheDriver) error {
 		return err
 	}
 	if !emptyUpper {
-		fmt.Println(fmt.Sprintf("Remove-%s: Clone Begin, %s", volumeName, buildCache.mergeDir))
+		fmt.Println(fmt.Sprintf("Unmount-%s: Clone Begin, %s", volumeName, buildCache.mergeDir))
 		if err = cloneDir(buildCache.mergeDir, getBasePath(buildCache.job, buildCache.build, driver.cacheLocations.cacheLowerRootDir)); err != nil {
-			fmt.Println(fmt.Sprintf("Remove-%s: Clone Dir failed %s", volumeName, err))
+			fmt.Println(fmt.Sprintf("Unmount-%s: Clone Dir failed %s", volumeName, err))
 			return err
 		} else {
 			cacheState, _ := getCacheState(driver.cacheLocations.cacheLowerRootDir)
 			cacheState.State[buildCache.job] = buildCache.build
 			cacheState.save(driver.cacheLocations.cacheLowerRootDir)
-			fmt.Println(fmt.Sprintf("Remove-%s: Clone complete. Cloned to %s", volumeName, driver.cacheLocations.cacheLowerRootDir))
+			fmt.Println(fmt.Sprintf("Unmount-%s: Clone complete. Cloned to %s", volumeName, driver.cacheLocations.cacheLowerRootDir))
 			return unMountVolume(buildCache)
 		}
 	} else {
-		fmt.Println(fmt.Sprintf("Remove-%s: Upper empty. cleaning up cache dirs", volumeName))
+		fmt.Println(fmt.Sprintf("Unmount-%s: Upper empty. cleaning up cache dirs", volumeName))
 		return unMountVolume(buildCache)
 	}
 }
 func unMountVolume(buildCache *buildCache) error {
 	volumeName := buildCache.job + "-" + buildCache.build
 	if err := syscall.Unmount(buildCache.mergeDir, 0); err != nil {
-		fmt.Println(fmt.Sprintf("Remove-%s: Syscall unmount %s failed. %s", volumeName, buildCache.mergeDir, err))
+		fmt.Println(fmt.Sprintf("Unmount-%s: Syscall unmount %s failed. %s", volumeName, buildCache.mergeDir, err))
 		return err
 	}
 	return nil
