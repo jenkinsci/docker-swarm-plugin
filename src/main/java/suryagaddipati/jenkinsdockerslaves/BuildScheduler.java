4f59787a271a624237ddb30191c467d551549028
Surya Gaddipati <surya.gaddipati@gmail.com>
2017-01-19 13:56:47 -0600

    Revert "Don't block queue thread to add a node"
    This reverts commit 6ac380d4a9f73bfeb6803dd8325789d673e47919.

--- src/main/java/suryagaddipati/jenkinsdockerslaves/BuildScheduler.java
+++ src/main/java/suryagaddipati/jenkinsdockerslaves/BuildScheduler.java
@@ -11,23 +11,27 @@ import java.io.IOException;
 
 public class BuildScheduler {
     public static void scheduleBuild(final Queue.BuildableItem bi, final boolean replace) {
-        Computer.threadPoolForRemoting.submit((Runnable) () -> {
-            try {
-                final DockerLabelAssignmentAction action = createLabelAssignmentAction();
-                if (replace) {
-                    bi.replaceAction(action);
-                } else {
-                    bi.addAction(action);
-                }
-
-                final Node node = new DockerSlave(bi, action.getLabel().toString());
-                Jenkins.getInstance().addNode(node);
-            } catch (final IOException e) {
-                e.printStackTrace();
-            } catch (final Descriptor.FormException e) {
-                e.printStackTrace();
+        try {
+            final DockerLabelAssignmentAction action = createLabelAssignmentAction();
+            if (replace) {
+                bi.replaceAction(action);
+            } else {
+                bi.addAction(action);
             }
-        });
+
+            final Node node = new DockerSlave(bi, action.getLabel().toString());
+            Computer.threadPoolForRemoting.submit((Runnable) () -> {
+                try {
+                    Jenkins.getInstance().addNode(node);
+                } catch (final IOException e) {
+//                        e.printStackTrace();
+                }
+            });
+        } catch (final IOException e) {
+            e.printStackTrace();
+        } catch (final Descriptor.FormException e) {
+            e.printStackTrace();
+        }
     }
 
     private static DockerLabelAssignmentAction createLabelAssignmentAction() {
