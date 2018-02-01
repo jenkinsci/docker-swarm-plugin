e84394b45ce6fc6fa8c33125da5acb57a9ce236b
Surya Gaddipati <sgaddipati@groupon.com>
2018-02-01 09:39:32 -0600

    add cloud from swarm.yml

--- src/main/java/suryagaddipati/jenkinsdockerslaves/DockerSwarmCloud.java
+++ src/main/java/suryagaddipati/jenkinsdockerslaves/DockerSwarmCloud.java
@@ -1,8 +1,12 @@
 package suryagaddipati.jenkinsdockerslaves;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import hudson.Extension;
+import hudson.init.InitMilestone;
+import hudson.init.Initializer;
 import hudson.model.Descriptor;
 import hudson.model.Label;
 import hudson.slaves.Cloud;
@@ -10,12 +14,19 @@ import hudson.slaves.NodeProvisioner;
 import jenkins.model.Jenkins;
 import org.kohsuke.stapler.DataBoundConstructor;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class DockerSwarmCloud extends Cloud {
     private static final String DOCKER_SWARM_CLOUD_NAME = "Docker Swarm";
+    private static final Logger LOGGER = Logger.getLogger(DockerSwarmCloud.class.getName());
     String dockerSwarmApiUrl;
     private String jenkinsUrl;
     private String swarmNetwork;
@@ -30,6 +41,11 @@ public class DockerSwarmCloud extends Cloud {
         this.swarmNetwork = swarmNetwork;
         this.cacheDriverName = cacheDriverName;
         this.agentTemplates = agentTemplates;
+    }
+
+    //for yaml serialization
+    public DockerSwarmCloud(){
+        super(DOCKER_SWARM_CLOUD_NAME);
     }
 
     @Override
@@ -89,6 +105,24 @@ public class DockerSwarmCloud extends Cloud {
 
     public void save(){
         getDescriptor().save();
+    }
+
+    @Initializer(after = InitMilestone.JOB_LOADED)
+    public static void initFromYaml() throws IOException {
+        File configsDir = new File(Jenkins.getInstance().getRootDir(), "pluginConfigs");
+        File swarmConfigYaml = new File(configsDir, "swarm.yml");
+        if (swarmConfigYaml.exists()) {
+            LOGGER.info("Configuring swarm plugin from " + swarmConfigYaml.getAbsolutePath());
+            ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+            try (InputStream in = new BufferedInputStream(new FileInputStream(swarmConfigYaml))) {
+                DockerSwarmCloud configuration = mapper.readValue(in, DockerSwarmCloud.class);
+                DockerSwarmCloud existingCloud = DockerSwarmCloud.get();
+                if(existingCloud != null){
+                    Jenkins.getInstance().clouds.remove(existingCloud);
+                }
+                Jenkins.getInstance().clouds.add(configuration);
+            }
+        }
     }
 }
 
--- src/main/java/suryagaddipati/jenkinsdockerslaves/DockerSwarmPlugin.java
+++ src/main/java/suryagaddipati/jenkinsdockerslaves/DockerSwarmPlugin.java
@@ -1,42 +1,18 @@
 package suryagaddipati.jenkinsdockerslaves;
 
 import akka.actor.ActorSystem;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.typesafe.config.ConfigFactory;
 import hudson.Extension;
 import hudson.Plugin;
-import jenkins.model.Jenkins;
-
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.util.logging.Logger;
 
 @Extension
 public class DockerSwarmPlugin extends Plugin {
-    private static final Logger LOGGER = Logger.getLogger(DockerSwarmPlugin.class.getName());
 
 
     private ActorSystem actorSystem;
 
     @Override
     public void start() throws Exception {
-
-        File configsDir = new File(Jenkins.getInstance().getRootDir(), "pluginConfigs");
-        File swarmConfigYaml = new File(configsDir,"swarm.yml");
-        if(swarmConfigYaml.exists()){
-            LOGGER.info("Configuring swarm plugin from " + swarmConfigYaml.getAbsolutePath());
-            try (InputStream in = new BufferedInputStream(new FileInputStream(swarmConfigYaml))) {
-                ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-//                DockerSwarmCloud configuration = mapper.readValue(in, DockerSwarmCloud.class);
-//                configuration.save();
-            }
-        }else {
-            LOGGER.info(swarmConfigYaml.getAbsolutePath() + " file not found.");
-        }
-
         this.actorSystem = ActorSystem.create("swarm-plugin", ConfigFactory.load());
     }
 
