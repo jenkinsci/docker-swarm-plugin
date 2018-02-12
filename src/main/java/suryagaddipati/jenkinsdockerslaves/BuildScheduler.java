1cead2807b0d57ab7e9818ad1a8d9a449dcd960a
Surya Gaddipati <sgaddipati@groupon.com>
2018-02-12 17:33:02 -0600

    add logging

--- src/main/java/suryagaddipati/jenkinsdockerslaves/BuildScheduler.java
+++ src/main/java/suryagaddipati/jenkinsdockerslaves/BuildScheduler.java
@@ -10,25 +10,26 @@ import hudson.security.ACLContext;
 import jenkins.model.Jenkins;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class BuildScheduler {
+    private static final Logger LOGGER = Logger.getLogger(BuildScheduler.class.getName());
     public static void scheduleBuild(final Queue.BuildableItem bi) {
         try (ACLContext _ = ACL.as(ACL.SYSTEM)) {
             final DockerLabelAssignmentAction action = createLabelAssignmentAction();
             final Node node = new DockerSlave(bi, action.getLabel().toString());
             bi.replaceAction(new DockerSlaveInfo(true));
             bi.replaceAction(action);
-            Computer.threadPoolForRemoting.submit((Runnable) () -> {
+            Computer.threadPoolForRemoting.submit(() -> {
                 try {
                     Jenkins.getInstance().addNode(node); //locks queue
                 } catch (final IOException e) {
-                    e.printStackTrace();
+                    LOGGER.log(Level.INFO,"couldn't add agent", e);
                 }
             });
-        } catch (final IOException e) {
-            e.printStackTrace();
-        } catch (final Descriptor.FormException e) {
-            e.printStackTrace();
+        } catch (final IOException|Descriptor.FormException e) {
+            LOGGER.log(Level.INFO,"couldn't add agent", e);
         }
     }
 
@@ -36,12 +37,11 @@ public class BuildScheduler {
         try {
             Thread.sleep(5, 10);
         } catch (final InterruptedException e) {
-            e.printStackTrace();
+            LOGGER.log(Level.INFO,"couldn't add agent", e);
         }
 
         final String id = "agent-" + System.nanoTime();
         final Label label = new DockerMachineLabel(id);
         return new DockerLabelAssignmentAction(label);
     }
-
 }
