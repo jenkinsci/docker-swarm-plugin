a7c0c696324fbcefa73b565e8d3781a9bea66b31
Surya Gaddipati <sgaddipati@groupon.com>
2018-03-19 16:56:51 -0500

    Only consider running tasks

--- src/main/java/org/jenkinsci/plugins/docker/swarm/docker/api/task/Status.java
+++ src/main/java/org/jenkinsci/plugins/docker/swarm/docker/api/task/Status.java
@@ -3,10 +3,10 @@ package org.jenkinsci.plugins.docker.swarm.docker.api.task;
 public class Status {
      public String State;
      public boolean isRunning(){
-         return "running".equals(State);
+         return "running".equalsIgnoreCase(State);
      }
 
     public boolean isComplete() {
-         return "complete".equals(State);
+         return "complete".equalsIgnoreCase(State);
     }
 }
