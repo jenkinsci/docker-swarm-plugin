d31b098767f8eacd130256f6bd4c5e29ca0815ea
Surya Gaddipati <sgaddipati@groupon.com>
2018-03-20 08:03:52 -0500

    Revert "Only consider running tasks"
    This reverts commit 2efc71ef22b91136c760ad1472db909759060ec0.

--- src/main/java/org/jenkinsci/plugins/docker/swarm/docker/api/task/Status.java
+++ src/main/java/org/jenkinsci/plugins/docker/swarm/docker/api/task/Status.java
@@ -3,10 +3,10 @@ package org.jenkinsci.plugins.docker.swarm.docker.api.task;
 public class Status {
      public String State;
      public boolean isRunning(){
-         return "running".equalsIgnoreCase(State);
+         return "running".equals(State);
      }
 
     public boolean isComplete() {
-         return "complete".equalsIgnoreCase(State);
+         return "complete".equals(State);
     }
 }
