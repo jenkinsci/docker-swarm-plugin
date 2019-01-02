5823cddffd436c1686c36b09b146bc5c44c53490
smhc <shane.hird@gmail.com>
2019-01-02 13:32:05 +1000

    Guarantee uniqueness of agent name

--- src/main/java/org/jenkinsci/plugins/docker/swarm/BuildScheduler.java
+++ src/main/java/org/jenkinsci/plugins/docker/swarm/BuildScheduler.java
@@ -11,9 +11,12 @@ import jenkins.model.Jenkins;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class BuildScheduler {
     private static final Logger LOGGER = Logger.getLogger(BuildScheduler.class.getName());
+    private static AtomicInteger counter = new AtomicInteger(1);
+
     public static void scheduleBuild(final Queue.BuildableItem bi) {
         try (ACLContext _ = ACL.as(ACL.SYSTEM)) {
             final DockerSwarmLabelAssignmentAction action = createLabelAssignmentAction(bi.task.getDisplayName());
@@ -35,18 +38,20 @@ public class BuildScheduler {
     }
 
     private static DockerSwarmLabelAssignmentAction createLabelAssignmentAction(String taskName) {
+
         try {
             Thread.sleep(5, 10);
         } catch (final InterruptedException e) {
             LOGGER.log(Level.INFO,"couldn't add agent", e);
         }
         taskName = taskName.replaceAll("[^a-zA-Z0-9]", "_");
-        if (taskName.length() > 15) {
-            taskName = taskName.substring(taskName.length() - 15);
+        if (taskName.length() > 30) {
+            taskName = taskName.substring(taskName.length() - 30);
         }
         String truncatedTime = Long.toString(System.nanoTime());
-        truncatedTime = truncatedTime.substring(truncatedTime.length() - 5);
-        
-        return new DockerSwarmLabelAssignmentAction("agent-" + taskName + "-" + truncatedTime);
+        truncatedTime = truncatedTime.substring(truncatedTime.length() - 10);
+
+        return new DockerSwarmLabelAssignmentAction("agent-" + taskName + "-" +
+                truncatedTime + "-" + BuildScheduler.counter.incrementAndGet());
     }
 }
